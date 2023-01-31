86c981e47d9a0487b67c2519f720520794aa8758
mehtabsinghnuance <mehtab.singh@nuance.com>
2023-01-31 13:33:20 -0500

    initial commit

--- Sources/MehtabPersonalLibrary/MehtabPersonalLibrary.swift
+++ Sources/MehtabPersonalLibrary/MehtabPersonalLibrary.swift
@@ -1,6 +1,7 @@
-public struct MehtabPersonalLibrary {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+class MehtabPersonalLibrary{
+    public func prettyPrint(toPrint printData: String) -> Void{
+        print("******************** logging Data *******************")
+        print(printData)
     }
 }
+
