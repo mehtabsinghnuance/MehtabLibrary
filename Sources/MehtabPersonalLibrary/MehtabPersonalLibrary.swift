fcd6f75d9afbf7d2153a87b6771aa3a506343a2f
mehtabsinghnuance <mehtab.singh@nuance.com>
2023-01-31 13:38:24 -0500

    initial commit

--- Sources/MehtabPersonalLibrary/MehtabPersonalLibrary.swift
+++ Sources/MehtabPersonalLibrary/MehtabPersonalLibrary.swift
@@ -1,4 +1,4 @@
-class MehtabPersonalLibrary{
+public class MehtabPersonalLibrary{
     public func prettyPrint(toPrint printData: String) -> Void{
         print("******************** logging Data *******************")
         print(printData)
