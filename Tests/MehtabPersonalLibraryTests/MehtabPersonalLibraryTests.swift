aec6efcae21b0c25069c34a00e59b52a91623214
mehtabsinghnuance <mehtab.singh@nuance.com>
2023-01-31 13:30:25 -0500

    Initial Commit

---
+++ Tests/MehtabPersonalLibraryTests/MehtabPersonalLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MehtabPersonalLibrary
+
+final class MehtabPersonalLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MehtabPersonalLibrary().text, "Hello, World!")
+    }
+}
