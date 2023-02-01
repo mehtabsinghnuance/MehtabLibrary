39dbb82d71d17e16cd580aae27c2673bb9c59d90
mehtabsinghnuance <mehtab.singh@nuance.com>
2023-02-01 08:10:43 -0500

    initial commit

--- Sources/MehtabPersonalLibrary/PrintUtility.swift
+++ Sources/MehtabPersonalLibrary/PrintUtility.swift
@@ -1,5 +1,15 @@
 public class PrintUtility{
+    
+    public init(){
+        print("Initializing Print Utility")
+    }
+    
     public func prettyPrint(toPrint printData: String) -> Void{
+        print("******************** logging Data *******************")
+        print(printData)
+    }
+    
+    public static func staticPrettyPrint(toPrint printData: String) -> Void{
         print("******************** logging Data *******************")
         print(printData)
     }
