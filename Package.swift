37465c9923ad4e7954e6f5f3a4b0764bcb674a15
pipeline <pipeline@nuance.com>
2023-02-06 10:45:11 -0500

    add

--- Package.swift
+++ Package.swift
@@ -20,6 +20,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
 //         .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/pointfreeco/swift-snapshot-testing",
+            from: "1.10.0"
+          ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -36,6 +40,6 @@ let package = Package(
                 path: "Sources/SecondLibrary"),
         .testTarget(
             name: "MehtabPersonalLibraryTests",
-            dependencies: ["MehtabPersonalLibrary"]),
+            dependencies: ["MehtabPersonalLibrary",  .product(name: "SnapshotTesting", package: "swift-snapshot-testing")]),
     ]
 )
